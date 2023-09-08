3ddb7bcd68fce97f6d75fd9eaa10cd103d678365
Will Hawkins <hawkinsw@obs.cr>
2023-09-07 23:03:38 -0400

    [Feature] Add periodic Endpoint
    Add an endpoint to the server that will stream an HTTP response _n_
    bytes per second, where _n_ is a parameter set in the query string
    (capped at a max of 4k).

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"sync"
 	"sync/atomic"
@@ -21,19 +22,25 @@ const (
 	smallContentLength int64 = 1
 	largeContentLength int64 = 8 * 1024 * 1024 * 1024
 	chunkSize          int64 = 64 * 1024
+	maxPeriodicSize    int64 = 4 * 1024
 
 	// ServiceType is the dns-sd service type for this service
 	ServiceType = "_nq._tcp"
 )
 
 var (
-	buffed []byte
+	chunkBuffed    []byte
+	periodicBuffed []byte
 )
 
 func init() {
-	buffed = make([]byte, chunkSize)
-	for i := range buffed {
-		buffed[i] = 'x'
+	chunkBuffed = make([]byte, chunkSize)
+	for i := range chunkBuffed {
+		chunkBuffed[i] = 'x'
+	}
+	periodicBuffed = make([]byte, maxPeriodicSize)
+	for i := range periodicBuffed {
+		periodicBuffed[i] = 'x'
 	}
 }
 
@@ -54,18 +61,20 @@ type handlers struct {
 func BulkHandlers(prefix string, EnableCORS bool) map[string]http.HandlerFunc {
 	h := &handlers{EnableCORS: EnableCORS}
 	return map[string]http.HandlerFunc{
-		prefix + "/small": h.smallHandler,
-		prefix + "/large": h.largeHandler,
-		prefix + "/slurp": h.slurpHandler,
+		prefix + "/small":    h.smallHandler,
+		prefix + "/large":    h.largeHandler,
+		prefix + "/slurp":    h.slurpHandler,
+		prefix + "/periodic": h.periodicHandler,
 	}
 }
 
 func CountingBulkHandlers(prefix string, EnableCORS bool, bytesServed, bytesReceived *uint64) map[string]http.HandlerFunc {
 	h := &handlers{EnableCORS: EnableCORS, BytesServed: bytesServed, BytesReceived: bytesReceived}
 	return map[string]http.HandlerFunc{
-		prefix + "/small": h.smallHandler,
-		prefix + "/large": h.largeHandler,
-		prefix + "/slurp": h.slurpHandler,
+		prefix + "/small":    h.smallHandler,
+		prefix + "/large":    h.largeHandler,
+		prefix + "/slurp":    h.slurpHandler,
+		prefix + "/periodic": h.periodicHandler,
 	}
 }
 
@@ -129,6 +138,7 @@ func (m *Server) generateConfig() {
 		SmallHTTPSDownloadURL string `json:"small_https_download_url"`
 		LargeHTTPSDownloadURL string `json:"large_https_download_url"`
 		HTTPSUploadURL        string `json:"https_upload_url"`
+		HTTPSPeriodicURL      string `json:"https_periodic_url"`
 	}{
 		SmallDownloadURL:      m.generateSmallDownloadURL(),
 		LargeDownloadURL:      m.generateLargeDownloadURL(),
@@ -136,6 +146,7 @@ func (m *Server) generateConfig() {
 		SmallHTTPSDownloadURL: m.generateSmallDownloadURL(),
 		LargeHTTPSDownloadURL: m.generateLargeDownloadURL(),
 		HTTPSUploadURL:        m.generateUploadURL(),
+		HTTPSPeriodicURL:      m.generatePeriodicURL(),
 	}
 
 	resp := struct {
@@ -189,6 +200,10 @@ func (m *Server) generateUploadURL() string {
 	return fmt.Sprintf("%s://%s%s/slurp", m.Scheme, m.PublicHostPort, m.ContextPath)
 }
 
+func (m *Server) generatePeriodicURL() string {
+	return fmt.Sprintf("%s://%s%s/periodic", m.Scheme, m.PublicHostPort, m.ContextPath)
+}
+
 func (h *handlers) smallHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet && r.Method != http.MethodHead {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -229,6 +244,37 @@ func (h *handlers) largeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (h *handlers) periodicHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet && r.Method != http.MethodHead {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	if !r.URL.Query().Has("size") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	periodicSize, err := strconv.ParseInt(r.URL.Query().Get("size"), 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Length", strconv.FormatInt(largeContentLength, 10))
+	w.Header().Set("Content-Type", "application/octet-stream")
+
+	if h.EnableCORS {
+		setCors(w.Header())
+	}
+
+	if r.Method != http.MethodGet {
+		return
+	}
+
+	if err := h.periodicBodyWriter(w, time.Second, periodicSize, largeContentLength); !ignorableError(err) {
+		log.Printf("Error periodically writing content of length %d: %s", largeContentLength, err)
+	}
+}
+
 func (h *handlers) chunkedBodyWriter(w http.ResponseWriter, contentLength int64) error {
 	w.WriteHeader(http.StatusOK)
 
@@ -238,20 +284,72 @@ func (h *handlers) chunkedBodyWriter(w http.ResponseWriter, contentLength int64)
 			n -= chunkSize
 			atomic.AddUint64(h.BytesServed, uint64(chunkSize))
 
-			if _, err := w.Write(buffed); err != nil {
+			if _, err := w.Write(chunkBuffed); err != nil {
 				return err
 			}
 			continue
 		}
 
 		atomic.AddUint64(h.BytesServed, uint64(n))
-		if _, err := w.Write(buffed[:n]); err != nil {
+		if _, err := w.Write(chunkBuffed[:n]); err != nil {
 			return err
 		}
 		break
 	}
 
 	return nil
+}
+
+func (h *handlers) periodicBodyWriter(w http.ResponseWriter, interval time.Duration, periodicSize int64, contentLength int64) error {
+	var err error = nil
+	w.WriteHeader(http.StatusOK)
+
+	periodicWriterWg := sync.WaitGroup{}
+
+	periodicSize = int64(math.Min(float64(periodicSize), float64(maxPeriodicSize)))
+
+	periodicWriterWg.Add(1)
+	go func() {
+		n := contentLength
+		for n > 0 {
+			// Send a _periodicSize_ amount of data every _interval_ period.
+			time.Sleep(interval)
+
+			// n >= chunkSize: We have at least a full _periodicSize_ amount of data left to send.
+			if n >= periodicSize {
+				n -= periodicSize
+				atomic.AddUint64(h.BytesServed, uint64(periodicSize))
+
+				if _, writeErr := w.Write(periodicBuffed[:periodicSize]); writeErr != nil {
+					err = writeErr
+					break
+				}
+				// Because we are attempting to detect buffer bloat, as soon
+				// as we have written data, we flush it to the network.
+				// In other words, we can't very well detect bloat if *we* are
+				// the source of it!
+				flusher, ok := w.(http.Flusher)
+				if ok {
+					flusher.Flush()
+				} else {
+					log.Printf("The http response writer is not flushable -- abnormal situation")
+				}
+				continue
+			}
+
+			// n < chunkSize: We have less than a full chunk left to send
+			atomic.AddUint64(h.BytesServed, uint64(n))
+			if _, writeErr := w.Write(periodicBuffed[:n]); writeErr != nil {
+				err = writeErr
+			}
+			break
+		}
+		periodicWriterWg.Done()
+	}()
+
+	periodicWriterWg.Wait()
+
+	return err
 }
 
 // setNoPublicCache tells the proxy to cache the content and the user
